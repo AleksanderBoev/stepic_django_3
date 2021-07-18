bbb2b59f0beff8fb060589734cde9ec796dac48d
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-18 21:02:21 +0300

    update3

--- vacancies/templatetags/filters.py
+++ vacancies/templatetags/filters.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.filter(name='null_to_no')
-def null_to_no(value):
+def null_to_no(value):#отслеживание и замена нулей на "нет"
     if str(value) == '0':
         return 'нет'
     return value
@@ -12,11 +12,8 @@ def null_to_no(value):
 @register.filter(name='rus_plural_endings')
 def rus_plural_endings(value, arg):
     if 5 <= abs(value) % 100 <= 20 or value == 0:
-        print('ий')
         return arg + 'ий'
     if abs(value) % 10 == 1:
-        print('ия')
         return arg+'ия'
     if 2 <= abs(value) % 10 <= 4:
-        print('ии')
         return arg + 'ии'
--- vacancies/views.py
+++ vacancies/views.py
@@ -2,35 +2,57 @@ from django.shortcuts import render
 from vacancies.models import Company
 from vacancies.models import Specialty
 from vacancies.models import Vacancy
-from django.db.models import Count, Variance
+from django.http import Http404, HttpResponseServerError
+
+
 
 def company_view(request, company_id):
-    company_data = Company.objects.get(id=company_id)
-    return render(request, 'companies/companies.html', {
-        'company': company_data,
-    })
+    try:
+        company_data = Company.objects.get(id=company_id)
+        return render(request, 'companies/companies.html', {
+            'company': company_data,
+        })
+    except Company.DoesNotExist:
+        raise Http404
+    except Exception as error:
+        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
+
 
 
 def main_view(request):
-    return render(request, 'index.html', {
-        'companies': Company.objects.all(),
-        'vacancies': Vacancy.objects.all(),
-        'specialty': Specialty.objects.all(),
-    })
+    try:
+        return render(request, 'index.html', {
+            'companies': Company.objects.all(),
+            'vacancies': Vacancy.objects.all(),
+            'specialty': Specialty.objects.all(),
+        })
+    except Exception as error:
+        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
 
 
 def vacancy_view(request, vacancy_id):
-    return render(request, 'vacancy/vacancy.html', {
-        'vacancy': Vacancy.objects.get(id=vacancy_id)
-    })
+    try:
+        return render(request, 'vacancy/vacancy.html', {
+            'vacancy': Vacancy.objects.get(id=vacancy_id)
+        })
+    except Vacancy.DoesNotExist:
+        raise Http404
+    except Exception as error:
+        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
 
 
 def vacancies_view(request, vacancies_path=None):
-    print(vacancies_path)
-    if vacancies_path is None:
-        vacancies = Vacancy.objects.all()
-    else:
-        vacancies = Vacancy.objects.filter(specialty__code=vacancies_path)
-    return render(request, 'vacancies/vacancies.html', {
-        'vacancies': vacancies,
-    })
+    try:
+        if vacancies_path is None:
+            vacancies = Vacancy.objects.all()
+        else:
+            vacancies = Vacancy.objects.filter(specialty__code=vacancies_path)
+            if vacancies.count() == 0:
+                raise Http404
+        return render(request, 'vacancies/vacancies.html', {
+            'vacancies': vacancies,
+        })
+    except Http404:
+        raise Http404
+    except Exception as error:
+        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
