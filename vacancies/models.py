391804c55d402397877002f708401837389b11d0
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-28 16:54:02 +0300

    2

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from datetime import datetime
 
 
 class Company(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
-    logo = models.URLField(default='https://place-hold.it/100x60')
+    logo = models.ImageField(upload_to='company_images/', null=True)
     description = models.TextField()
     employee_count = models.IntegerField()
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='Company')
 
     class Meta:
         app_label = 'vacancies'
@@ -15,7 +19,7 @@ class Company(models.Model):
 class Specialty(models.Model):
     code = models.CharField(max_length=15)
     title = models.CharField(max_length=20)
-    picture = models.URLField(default='https://place-hold.it/100x60')
+    picture = models.ImageField(upload_to='speciality_images/')
 
     class Meta:
         app_label = 'vacancies'
@@ -29,7 +33,15 @@ class Vacancy(models.Model):
     description = models.TextField()
     salary_min = models.FloatField()
     salary_max = models.FloatField()
-    published_at = models.DateField()
+    published_at = models.DateField(default=datetime.now())
 
     class Meta:
         app_label = 'vacancies'
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=20)
+    written_phone = models.CharField(max_length=20)
+    written_cover_letter = models.TextField()
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name="applications")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="applications")
--- vacancies/views.py
+++ vacancies/views.py
@@ -3,8 +3,77 @@ from vacancies.models import Company
 from vacancies.models import Specialty
 from vacancies.models import Vacancy
 from django.http import Http404, HttpResponseServerError
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+import django_vacancies.settings as settings
+from django.contrib.auth.models import User
+from django import forms
+from django.views.generic import ListView
 
+def mycompany_vacancy_create_view(request, **kwargs):
+    return render(request, 'companies/vacancy-edit.html', {
+            'company': updata_data.cleaned_data,
+        })
 
+class mycompany_vacancies_view(ListView):
+    model = Vacancy
+    template_name ='companies/vacancy-list.html'
+    def get_queryset(self):
+        return Vacancy.objects.filter(company__owner__username=self.request.user)
+
+class mycompany_edit_form(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ('name',
+                  'location',
+                  #'logo',
+                  'description',
+                  'employee_count',
+                  )
+
+def mycompany_edit_view(request, **kwargs):
+    data = Company.objects.filter(owner=User.objects.get(username=request.user))
+    #print('ddd',data.first().owner)
+    if request.method.upper() == 'POST':
+        updata_data = mycompany_edit_form(request.POST)
+        if updata_data.is_valid():
+            updata_data = updata_data.save(commit=False)
+            updata_data.owner = User.objects.get_by_natural_key(username=request.user)
+            updata_data.logo = request.FILES.get('logo') or 
+            if data.count():
+                print('logo', updata_data.logo)
+                Company.objects.filter(owner=User.objects.get(username=request.user)).update(
+                    name=updata_data.name,
+                    location=updata_data.location,
+                    logo=updata_data.logo or data.first().logo,
+                    description=updata_data.description,
+                    employee_count=updata_data.employee_count,
+                    owner=User.objects.get(username=request.user),
+                )
+            else:
+                updata_data.save()
+            reneder_data = updata_data
+        else:
+            print(repr(updata_data.errors))
+            reneder_data = updata_data.cleaned_data
+        return render(request, 'companies/company-edit.html', {
+            'company': reneder_data,
+            'form': updata_data,
+        })
+    print(request.META.get('HTTP_REFERER'))
+    if data.count() == 0 and '/mycompany/letsstart' not in (request.META.get('HTTP_REFERER') or ''):
+        return HttpResponseRedirect('/mycompany/letsstart')
+    elif kwargs['new'] and data.count():
+        return HttpResponseRedirect('/mycompany')
+    return render(request, 'companies/company-edit.html', {
+        'company': data.first(),
+        'form': mycompany_edit_form,
+    })
+
+def mycompany_letsstart_view(request):
+    return render(request, 'companies/company-create.html')
+def mycompany_vacancy_view(request):
+    return render(request, 'companies/company-create.html')
 
 def company_view(request, company_id):
     try:
@@ -14,31 +83,32 @@ def company_view(request, company_id):
         })
     except Company.DoesNotExist:
         raise Http404
-    except Exception as error:
-        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
 
 
 
 def main_view(request):
-    try:
-        return render(request, 'index.html', {
-            'companies': Company.objects.all(),
-            'vacancies': Vacancy.objects.all(),
-            'specialty': Specialty.objects.all(),
-        })
-    except Exception as error:
-        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
+    print(User.objects.filter(username=request.user).count())
+    return render(request, 'index.html', {
+        'companies': Company.objects.all(),
+        'vacancies': Vacancy.objects.all(),
+        'specialty': Specialty.objects.all(),
+    })
 
 
 def vacancy_view(request, vacancy_id):
+    if request.method.upper() == 'POST':
+        print('sss')
+        print(request.POST.get('userName'))
+        return render(request, 'vacancy/send.html', {
+            'vacancy_id': vacancy_id,
+            'picture_rout': settings.MEDIA_URL + '/check.png',
+        })
     try:
         return render(request, 'vacancy/vacancy.html', {
-            'vacancy': Vacancy.objects.get(id=vacancy_id)
+            'vacancy': Vacancy.objects.get(id=vacancy_id),
         })
     except Vacancy.DoesNotExist:
         raise Http404
-    except Exception as error:
-        return HttpResponseServerError('''<br><h1>The error on server</h1>''')
 
 
 def vacancies_view(request, vacancies_path=None):
