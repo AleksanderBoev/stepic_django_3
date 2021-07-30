f285f1bcc3558210a90bf139078660e0e50c6eef
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-30 18:30:12 +0300

    3

--- django_vacancies/settings.py
+++ django_vacancies/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!2e=q4(svv-mq-se_)-(dr9p-a-(m#l3vs=fkuv1rmq@_a)y45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vacancies',
     'accounts',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
