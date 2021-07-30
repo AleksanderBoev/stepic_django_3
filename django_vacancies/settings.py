5b2514cb439105e258e32c3c1314a04de486c1f9
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-30 18:57:07 +0300

    3

--- django_vacancies/settings.py
+++ django_vacancies/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!2e=q4(svv-mq-se_)-(dr9p-a-(m#l3vs=fkuv1rmq@_a)y45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://stepic-django-3.herokuapp.com/']
 
--- django_vacancies/urls.py
+++ django_vacancies/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path('mycompany/letsstart/', mycompany_letsstart_view, name='mycompany_letsstart'),
     path('mycompany/create/', mycompany_edit_view, {'new': True}, name='mycompany_create'),
     path('mycompany', mycompany_edit_view, {'new': False}, name='mycompany_edit'),
-    path('/mycompany/vacancies/', mycompany_vacancies_view.as_view(), name='mycompany_vacancies'),
-    path('/mycompany/vacancies/create/', mycompany_vacancy_create_view, {'new': True}, name='mycompany_vacancy_create'),
-    path('/mycompany/vacancies/<int:vacancy_id>', mycompany_vacancy_create_view, {'new': False}, name='mycompany_vacancy'),
+    path('mycompany/vacancies/', mycompany_vacancies_view.as_view(), name='mycompany_vacancies'),
+    path('mycompany/vacancies/create/', mycompany_vacancy_create_view, {'new': True}, name='mycompany_vacancy_create'),
+    path('mycompany/vacancies/<int:vacancy_id>', mycompany_vacancy_create_view, {'new': False}, name='mycompany_vacancy'),
 ]
 
 if settings.DEBUG:
