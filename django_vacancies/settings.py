39accb825ec835f59acd4aafcbf0af6fe40e877d
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-30 18:53:14 +0300

    3

--- django_vacancies/settings.py
+++ django_vacancies/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!2e=q4(svv-mq-se_)-(dr9p-a-(m#l3vs=fkuv1rmq@_a)y45
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['https://stepic-django-3.herokuapp.com/']
 
 
 # Application definition
